db1b2e0b00b1c69711665990bcd8dd27fecfac22
ee250002084-spec <ee250002084@iiti.ac.in>
2025-09-08 15:19:40 +0530

    Update question2.cpp

--- question2.cpp
+++ question2.cpp
@@ -1,51 +1,58 @@
 #include <iostream>
 #include <fstream>
-#include <cstring>
+#include <string>
 
-using namespace std;
-
-// TODO: Define the Rectangle struct according to README
-// struct Rectangle {
-// };
+struct Rectangle {
+    double width;
+    double height;
+    double thickness;
+    double density;
+    std::string material;
+};
 
 void print_plate(int index, double width, double height,
                  double density, double mass, const char* material) {
-    cout << "Plate[" << index << "]: "
-         << "width=" << width << ", "
-         << "height=" << height << ", "
-         << "density=" << density << ", "
-         << "mass=" << mass << ", "
-         << "material=" << material << "\n";
+    std::cout << "Plate[" << index << "]: "
+              << "width=" << width << ", "
+              << "height=" << height << ", "
+              << "density=" << density << ", "
+              << "mass=" << mass << ", "
+              << "material=" << material << "\n";
 }
 
-int main(int argc, char* argv[]) {
-    if (argc < 2) {
-        cerr << "Usage: " << argv[0] << " <input_file>" << endl;
+int main() {
+    std::ifstream infile("plates.txt");
+    if (!infile) {
+        std::cerr << "Error opening file.\n";
         return 1;
     }
 
-    ifstream input(argv[1]);
-    if (!input) {
-        cerr << "Error opening " << argv[1] << endl;
-        return 1;
+    int n;
+    infile >> n;
+    infile.ignore();
+
+    Rectangle* plates = new Rectangle[n];
+
+    for (int i = 0; i < n; ++i) {
+        infile >> plates[i].width;
+        infile >> plates[i].height;
+        infile >> plates[i].thickness;
+        infile >> plates[i].density;
+        infile.ignore();  // consume newline before reading material string
+        std::getline(infile, plates[i].material);
     }
 
-    int num_plates;
-    input >> num_plates;
-    input.ignore(); // skip newline after number
+    for (int i = 0; i < n; ++i) {
+        double mass = plates[i].width * plates[i].height * plates[i].thickness * plates[i].density;
+        print_plate(i,
+                    plates[i].width,
+                    plates[i].height,
+                    plates[i].density,
+                    mass,
+                    plates[i].material.c_str());
+    }
 
-    const int MAX_PLATES = 10;
-
-    // TODO: Create an array of Rectangle
-    // Rectangle plates[MAX_PLATES];
-
-    // TODO: Read plate data from input
-    // for (int i = 0; i < num_plates; i++) {
-    // }
-
-    // TODO: Compute mass for each plate and call print_plate
-    // for (int i = 0; i < num_plates; i++) {
-    // }
-
+    delete[] plates;
     return 0;
 }
+
